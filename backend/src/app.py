e43d430a1dde1019b2565e4dcf6998b4b380b99d
thygolem <thygolem@gmail.com>
2021-07-07 11:22:19 +0200

    creando las 5 rutas en flask

---
+++ backend/src/app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['MONGO_URI']='mongodb://localhost/pythonreactdb'
+mongo = PyMongo(app)
+
+db = mongo.db.users
+
+@app.route('/users', methods=['POST'])
+def createUser():
+    return 'OK'
+
+@app.route('/users', methods=['GET'])
+def getUsers():
+    return 'OK'
+
+@app.route('/users/<id>', methods=['GET'])
+def getUser(id):
+    return 'OK'
+
+@app.route('/users/<id>', methods=['DELETE'])
+def deleteUser(id):
+    return 'OK'
+
+@app.route('/users/<id>', methods=['PUT'])
+def updateUser(id):
+    return 'OK'
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
